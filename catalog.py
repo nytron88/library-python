ed86b8c4cf794aba925246b261f0ee212a2413fa
nytron88 <79620454+nytron88@users.noreply.github.com>
2022-06-29 10:17:28 -0500

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -1,14 +1,12 @@
+from booksinlibrary import Books
 from book import Book
-from BooksInLibrary import Books
-
 
 class Catalog:
     def __init__(self):
-        self.books = [Books.book, Books.book2, Books.book3]
+        self.books = [Books.book1, Books.book2, Books.book3]
 
     def addbook(self, Book):
         self.books.append(Book)
 
     def removebook(self, Book):
         self.books.remove(Book)
-
