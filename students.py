676a5534ee0dfedf05714feb7138201fb49db9b9
nytron88 <79620454+nytron88@users.noreply.github.com>
2022-06-29 10:19:13 -0500

    Create students.py

---
+++ students.py
@@ -0,0 +1,7 @@
+from student import Student
+
+class students:
+    student1 = Student("Sid", 1)
+    student2 = Student("Adi", 2)
+    student3 = Student("Raj", 3)
+    student4 = Student("Anu", 4)
