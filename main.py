5598f1b1556676ffde24059cf7e981f5fae57767
nytron88 <79620454+nytron88@users.noreply.github.com>
2022-06-29 10:16:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,94 @@
+from library import Library
 from book import Book
 from student import Student
-from library import Library
-from catalog import Catalog
 
-catalog = Catalog()
-lib = Library(catalog)
-
-id = input("id: ")
-passw = (input("Passcode: "))
+lib = Library()
+stud = ""
 done = False
+logged = False
 
-if id == "AdiJai8334" and int(passw) == 1111:
+print("Welcome to my Library System \n")
+sign = input("Sign 'up' or Sign 'in': ")
 
-    stud = Student("Sid", 1)
-    stud2 = Student("Adi", 2)
+while not done:
+    if sign == "up":
+        up_name = input("What's your name: ")
+        up_roll = int(input("Choose a roll number: "))
+        stud = Student(up_name, up_roll)
+        checker = False
+        for i in lib.students:
+            if stud.__eq__(i):
+                print("Change roll number please")
+                checker = True
+                done = True
+                break
+            else:
+                checker = False
+        if not checker:
+            print("Success")
+            lib.students.append(stud)
+            done = True
+            logged = True
+    elif sign == "in":
+        in_name = input("What's your name: ")
+        in_roll = int(input("What's your roll number: "))
+        stud = Student(in_name, in_roll)
+        for i in lib.students:
+            if stud.__eq__(i):
+                print("Successfully signed in!")
+                done = True
+                logged = True
+                break
+            else:
+                print("Invalid login")
+                done = True
+                break
 
-    who_name = input("Your Name: ")
-    who_rollnumber = int(input("Your roll number: "))
-
-    print("You Logged in as a student")
+if logged:
+    print()
     print("'lib' to check what books are available in the library. 'details' to check what books are checked out "
-          "to you. 'checkout' to checkout a book. 'checkin' to return a book to the library")
+          "to you. 'checkout' to checkout a book. 'checkin' to return a book to the library. 'done' to end the program \n")
 
-    stud_1 = False
+while logged:
+    command = input("Type in the command you want to use: ")
 
-    if who_name == stud.name and who_rollnumber == stud.roll_number:
-        stud_1 = True
-    elif who_name == stud2.name and who_rollnumber == stud2.roll_number:
-        stud_1 = False
+    if command.lower() == "lib":
+        print(lib.checkitems())
+
+    elif command.lower() == "checkout":
+        print(lib.checkitems())
+        count = int(input("How many books: "))
+        i = 0
+        while i < count:
+            name = input("Book name: ")
+            author = input("Author: ")
+            book = Book(name, author)
+            lib.checkout(stud, book)
+            print(lib.itemsoutStu(stud))
+            print("Successfully checkedout")
+            i += 1
+
+    elif command.lower() == "checkin":
+        f"\n These items are checkedout to you: {lib.itemsoutStu(stud)} \n"
+        count = int(input("How many books: "))
+        print("Which ones: ")
+        i = 0
+        while i < count:
+            name = input("Book name: ")
+            author = input("Author: ")
+            book = Book(name, author)
+            lib.checkin(stud, book)
+            print(lib.itemsoutStu(stud))
+            print("Successfully checkedin")
+            i += 1
+
+    elif command.lower() == "done":
+        print("Thanks for using my library system!")
+        logged = False
+        break
+
+    elif command.lower() == "details":
+        print(lib.itemsoutStu(stud))
+
     else:
-        done = True
-        print("Wrong student, restart")
-    while not done:
-        command = input("\n Type in the command you want to use: ")
-
-        if command.lower() == 'checkout':
-            book_name = input("Book name: ")
-            book_author = input("Author name: ")
-            book = Book(book_name, book_author)
-            if stud_1:
-                lib.checkout(stud, book)
-            else:
-                lib.checkout(stud2, book)
-
-        elif command.lower() == 'lib':
-            print(lib.Catalog.books)
-
-        elif command.lower() == 'checkin':
-            book_name = input("Book name: ")
-            book_author = input("Author name: ")
-            book = Book(book_name, book_author)
-            if stud_1:
-                lib.checkIn(stud, book)
-            else:
-                lib.checkIn(stud2, book)
-        elif command.lower() == 'details':
-            if stud_1:
-                print(lib.checkItems(stud))
-            else:
-                print(lib.checkItems(stud2))
-
-elif id == "SidJai6944" and int(passw) == 0000:
-    print("You Logged in as a teacher")
-
-else:
-    print("Login failed, please try again")
-    done = True
+        print("Error command please try again")
